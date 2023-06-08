41b75169c076a21822b102b95ae7cefa31968e4e
Satyia Anand <35591840+strydhr@users.noreply.github.com>
2023-06-08 15:12:01 +0800

    Update Constants.kt

--- app/src/main/java/com/strydhr/thepasar/Utilities/Constants.kt
+++ app/src/main/java/com/strydhr/thepasar/Utilities/Constants.kt
@@ -6,7 +6,7 @@ import com.google.firebase.storage.ktx.storage
 import com.strydhr.thepasar.Model.User
 val serverApi = "AAAAUJA7Y08:APA91bG1yX3iyxgdhD3jFB-ag3TlD6HxixV_GAR3aOhz-QoIU7a2s51I_GpWUsCXiRaXSXvguU2kIysHmJfVK5wWlgpC2L17j9ROxba_avFWQ9ciVPZp9U-Aht95BMyGj0Jx2A_cDPz0"
 val serverKey = "key=" + serverApi
-val googleApi = "AIzaSyDUCD9JHOWuWojooiuumVdLMBFSdQVV73w"
+val googleApi = googlekey
 
 val db = Firebase.firestore
 val storage = Firebase.storage
